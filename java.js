5927e516fc8dc404e311fcf312f276db5c5488f4
Lakhyajyoti Phukon <phukon066@gmail.com>
2026-09-15 16:34:22 +0530

    initial

---
+++ java.js
@@ -0,0 +1,9 @@
+//let age=20;
+//console.log(age);
+
+//let borishkaage=17;
+//let collegekaage=25;
+//console.log(borishkaage + collegekaage)
+
+let arr=["banna","apple","mango",23,55,78];
+console.log(arr)
---
+++ simple.js
@@ -0,0 +1,15 @@
+//function greet(){
+   // console.log("phukon")
+//}
+//greet()
+
+//function phukon(a,b){
+   // console.log(a+b)
+//}
+//phukon(100,200)
+
+function add(a,b){
+    return a+b;
+}
+let result=add(50,6);
+console.log(result);
