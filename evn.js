50431ede9d8c3df33d062443d8f7c6d95c28b0b8
Lakhyajyoti Phukon <phukon066@gmail.com>
2026-09-22 15:03:27 +0530

    myself

--- evn.js
+++ evn.js
@@ -11,24 +11,10 @@ function countEvenNumbers(numbers) {
   return count;
 }
 
-console.log(countEvenNumbers([3, 8, 11, 14, 20])); // 3
-console.log(countEvenNumbers([1, 3, 5])); // 0
-console.log(countEvenNumbers([2, 4, 6])); // 3
+console.log(countEvenNumbers([3, 8, 11, 14, 20]));
+console.log(countEvenNumbers([1, 3, 5]));
+console.log(countEvenNumbers([2, 4, 6]));
 
-
-
-function countEvenNumbers(numbers){
-    let count=0;
-
-    for(let i=0;i<numbers.length;i++){
-        if(numbers[i] % 2 !==0){
-            continue;
-        }
-        count ++;
-    }
-    return count;
-}
-console.log(countEvenNumbers([2, 4, 6])); 
 
 
 
